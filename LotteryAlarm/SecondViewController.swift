3a59e69f0a9c10037b08e6419869273d5f2e266d
中口翔太 <hare.tokidoki.kumori227@gmail.com>
2020-03-24 22:17:21 +0900

    Initial Commit

---
+++ LotteryAlarm/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LotteryAlarm
+//
+//  Created by 中口翔太 on 2020/03/24.
+//  Copyright © 2020 中口翔太. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
