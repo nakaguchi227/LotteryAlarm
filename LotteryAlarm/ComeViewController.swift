6cdb40849c2ca859ca8f77d30a660a61f313a8b0
中口翔太 <hare.tokidoki.kumori227@gmail.com>
2020-04-05 20:04:49 +0900

    クラス追加

---
+++ LotteryAlarm/ComeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComeViewController.swift
+//  LotteryAlarm
+//
+//  Created by 中口翔太 on 2020/04/05.
+//  Copyright © 2020 中口翔太. All rights reserved.
+//
+
+import UIKit
+
+class ComeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
